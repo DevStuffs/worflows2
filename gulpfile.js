014fe6ffec5209f39916bc6e528e5092536fbbfa
Sean Murphy <smokebreak@gmail.com>
2017-07-31 15:44:25 -0400

    Added static files- json and html to live reload monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ var jsSources = [
   'components/scripts/template.js'
 ];
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 gulp.task('coffee', function() {
   gulp.src(coffeeSources)
@@ -47,6 +49,8 @@ gulp.task('watch', function() {
   gulp.watch(coffeeSources, ['coffee']); // when something changes in the coffeeSources, then execute 'coffee'
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
+  gulp.watch(htmlSources, ['html']);
+  gulp.watch(jsonSources, ['json']);
 });
 
 gulp.task('connect', function() {
@@ -55,7 +59,18 @@ gulp.task('connect', function() {
     livereload: true // need to add the "reload()" method by piping it at the end of the js and compass task
   });
 })
-gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']); // naming this default allows you to simple run "gulp" in the terminal, rather than running each one as "gulp js, gulp-coffee", etc
+
+gulp.task('html', function() {
+    gulp.src(htmlSources)
+    .pipe(connect.reload())
+});
+
+gulp.task('json', function() {
+    gulp.src(jsonSources)
+    .pipe(connect.reload())
+});
+
+gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']); // naming this default allows you to simple run "gulp" in the terminal, rather than running each one as "gulp js, gulp-coffee", etc
 // test log
 // gulp.task('log', function() {
 //   gutil.log('Workflows are awesome');
